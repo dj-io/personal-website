4cd199fe66d1a1d9a8e6289190026442cf4e6ba6
Daquanj <Daquanj.dev@gmail.com>
2024-11-07 01:34:17 -0500

    configuring lambda->gateway for php email

--- php/send.php
+++ php/send.php
@@ -13,29 +13,29 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'email-smtp.us-east-1.amazonaws.com';   //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'AKIAQ2HLFZ6HNBZKAR55';                 //SMTP username
-    $mail->Password   = 'BHy7L/IvwxWRT5QZujetfogs7I3AZ+z7gaLb33ssbzK0'; //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+    $mail->isSMTP(); //Send using SMTP
+    $mail->Host       = $_ENV['SMTP_HOST']; //Set the SMTP server to send through
+    $mail->SMTPAuth   = true; //Enable SMTP authentication
+    $mail->Username   = $_ENV['SMTP_USERNAME']; //SMTP username
+    $mail->Password   = $_ENV['SMTP_PASSWORD']; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;//Enable implicit TLS encryption
+    $mail->Port       = 587; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('daquanj11@gmail.com', "$_POST[name]");
+    $mail->setFrom($_ENV['SMTP_SENDER'], "$_POST[name]");
     // $mail->addAddress('daquanj.dev@gmail.com', 'Daqua-Dev'); //Add a recipient
-    $mail->addAddress('daquanj.dev@gmail.com');                //Add a recipient (Name is optional)
+    $mail->addAddress($_ENV['SMTP_RECIPIENT']); //Add a recipient (Name is optional)
     $mail->addReplyTo("$_POST[email]", 'Recruiters and Hiring Managers are messaging you - Daquan : ');
     // $mail->addCC('cc@example.com');
     // $mail->addBCC('bcc@example.com');
 
     //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    // $mail->addAttachment('/var/tmp/file.tar.gz'); //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->isHTML(true); //Set email format to HTML
     $mail->Subject = "$_POST[subject]";
     $mail->Body    = "This is an HTML Body -- <h1>Yup</h1>";
     $mail->AltBody = "$_POST[message]";
